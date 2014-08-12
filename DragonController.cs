b82ab9620337e3ef12d99b879c602091b2a452ee
Pythagean <jeremysto@gmail.com>
2014-08-13 07:49:00 +1200

    Modified Dragon Controller, changed files to .cs

---
+++ DragonController.cs
@@ -0,0 +1,150 @@
+using UnityEngine;
+using System.Collections;
+
+public class DragonController : MonoBehaviour {
+
+	//Control Scheme
+	public KeyCode Left = KeyCode.A;
+	public KeyCode Right = KeyCode.D;
+	public KeyCode Jump = KeyCode.W;
+	public KeyCode Glide = KeyCode.Q;
+	public KeyCode FreeFall = KeyCode.S;
+
+	//Maximum stat values
+	public float healthBar = 200f;
+	public float staminaBar = 100f;
+	public float fireBar = 100f;
+
+	//Current stat values
+	private float currentHealth = healthBar;
+	private float currentStamina = staminaBar;
+	private float currentFire = fireBar;
+
+	//Rate at which stats regenerate
+	public float healthRegen = 5f;
+	public float staminaRegen = 10f;
+	public float fireRegen = 10f;
+
+	//Amount of stamina used by each activity
+	public float staminaUsageJump = 10f;
+	public float staminaUsageFlap = 5f;
+	public float staminaUsageGlide = 2f;
+
+	//Amount of fire used by each activity
+	public float fireUsageFireball = 10f;
+	public float fireUsageFlamethrower = 20f;
+
+	//Global variables for movement
+	public float gravity = -15f;
+	public float runSpeed = 5f;
+	public float decaySpeed = 0.5f;
+	public float jumpHeight = 2.5f;
+	public float flapHeight = 1.5f;
+	public float glidingSpeed = 5f;
+
+	private CharacterController2D _controller;
+	private Animator _animator;
+
+	//Boolean values storing current state of dragon
+	private bool isGliding;
+
+	void Awake()
+	{
+		_controller = GetComponent<CharacterController2D> ();
+		_animator = GetComponent<Animator> ();
+
+	}
+
+	void Update()
+	{
+		//Grabs current velocity of Dragon
+		var velocity = _controller.velocity;
+		//var stamina = staminaBar;
+
+		//Sets velocity.y to 0 and regenerates stamina when on ground
+		if (_controller.isGrounded)
+		{
+			velocity.y = 0;
+			if(currentStamina > staminaBar - 1)
+				currentStamina = staminaBar;
+			else
+				currentStamina += currentStamina * Time.deltaTime;
+		}
+
+		//Horizontal Input
+		if (Input.GetKey (Right)) 
+		{
+			velocity.x = runSpeed;
+			//goRight();
+		}
+		else if( Input.GetKey(Left))
+		{
+			velocity.x = -runSpeed;
+			//goLeft();
+		}
+
+		//Adds decay speed to the x velocity to slow dragon down
+		if(velocity.x < 0.001 && velocity.x > -0.001)
+			velocity.x = 0;
+		else
+			velocity.x *= decaySpeed;
+	
+		//Jumping from the ground
+		if (Input.GetKeyDown (Jump) && _controller.isGrounded && currentStamina > staminaUsageJump) 
+		{
+			var targetJumpHeight = jumpHeight;
+			velocity.y = Mathf.Sqrt(2f * targetJumpHeight * -gravity);
+			currentStamina -= staminaUsageJump;
+		}
+
+		//Flapping wings while in the air
+		if(Input.GetKeyDown(Jump) && !_controller.isGrounded && currentStamina > staminaUsageFlap)
+		{
+			var targetFlapHeight = flapHeight;
+			velocity.y = Mathf.Sqrt(2f * targetFlapHeight * -gravity);
+			currentStamina -= staminaUsageFlap;
+		}
+
+		//Initiate glide when in the air
+		if(Input.GetKeyDown(Glide) && !_controller.isGrounded && currentStamina > staminaUsageGlide)
+		{
+			velocity.y += glidingSpeed * Time.deltaTime;
+			currentStamina -= staminaUsageGlide;
+		}
+
+		//Initiate freefall from glide
+		if(Input.GetKeyDown(FreeFall) && !_controller.isGrounded && !isGliding)
+		{
+			velocity.y -= glidingSpeed * Time.deltaTime;
+		}
+
+		//Apply gravity to y velocity
+		velocity.y += gravity * Time.deltaTime;
+
+		onGUI();
+
+		//Move dragon using the DragonCharacterController2D Script
+		_controller.move(velocity * Time.deltaTime);
+	}
+
+	function onGUI()
+	{
+		GUI.Label(Rect(0,0,100,100),"Health: " + currentHealth.toString() + " / " + healthBar.toString());
+		GUI.Label(Rect(0,0,100,100),"Stamina: " + currentStamina.toString() + " / " + staminaBar.toString());
+		GUI.Label(Rect(0,0,100,100),"Fire: " + currentFire.toString() + " / " + fireBar.toString());
+		GUI.Label(Rect(0,0,100,100),"Is Grounded: " + isGrounded.toString());
+		GUI.Label(Rect(0,0,100,100),"Is Gliding: " + isGliding.toString());
+	}
+
+	private void goLeft()
+	{
+		if (transform.localScale.x > 0f)
+			transform.localScale = new Vector3 (-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+	}
+
+	private void goRight()
+	{
+		if (transform.localScale.x < 0f)
+			transform.localScale = new Vector3 (-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+	}
+}
---
+++ PlayerController.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public KeyCode Left = KeyCode.LeftArrow;
+	public KeyCode Right = KeyCode.RightArrow;
+	public KeyCode Jump = KeyCode.Space;
+	
+	public float healthBar = 100f;
+	public float staminaBar = 100f;
+
+	public float gravity = -15f;
+	public float runSpeed = 8f;
+	public float decaySpeed = 0.5f;
+	public float jumpHeight = 1.8f;
+
+	private CharacterController2D _controller;
+	private Animator _animator;
+
+	void Awake()
+	{
+		_controller = GetComponent<CharacterController2D> ();
+		_animator = GetComponent<Animator> ();
+
+	}
+
+	void Update()
+	{
+		//Grabs current velocity of player
+		var velocity = _controller.velocity;
+
+		//Debug.Log ("X Velocity: " + _controller.velocity.x);
+		//Debug.Log ("Y Velocity: " + _controller.velocity.y);
+
+		if (_controller.isGrounded)
+				velocity.y = 0;
+
+		//Horizontal Input
+		if (Input.GetKey (Right)) 
+		{
+			velocity.x = runSpeed;
+			//goRight();
+		}
+		else if( Input.GetKey(Left))
+		{
+			velocity.x = -runSpeed;
+			//goLeft();
+		}
+
+		if(velocity.x < 0.001 && velocity.x > -0.001)
+		{
+			velocity.x = 0;
+		}
+		else
+		{
+			velocity.x *= decaySpeed;
+		}
+		
+		if (Input.GetKeyDown (Jump) && _controller.isGrounded) 
+		{
+			var targetJumpHeight = jumpHeight;
+			velocity.y = Mathf.Sqrt(2f * targetJumpHeight * -gravity);
+			 
+
+		}
+
+		velocity.y += gravity * Time.deltaTime;
+
+		_controller.move(velocity * Time.deltaTime);
+	}
+
+	private void goLeft()
+	{
+		if (transform.localScale.x > 0f)
+			transform.localScale = new Vector3 (-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+	}
+
+	private void goRight()
+	{
+		if (transform.localScale.x < 0f)
+			transform.localScale = new Vector3 (-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+	}
+}
